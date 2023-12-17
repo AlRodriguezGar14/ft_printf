72a47b6073eec25de9e386a74dde9d9ea76a24c7
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-12-17 22:57:10 +0100

    printf function started. Addapted the functions putnbr, putsr and putchar to match the requirements

---
+++ ft_printf.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alberrod <alberrod@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 18:33:47 by alberrod          #+#    #+#             */
+/*   Updated: 2023/12/17 22:55:26 by alberrod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include "./libft/libft.h"
+
+
+ssize_t	ft_printf(char const *container, ...)
+{
+	va_list	args;
+	size_t	len;
+
+	va_start(args, container);
+
+	len = 0;
+	while (*container)
+	{
+		if (*container == '%')
+		{
+			container++;
+			if (*container == 'd')
+				len += ft_putnbr(va_arg(args, int));
+			if (*container == 's')
+				len += ft_putstr(va_arg(args, char *));
+					
+		}
+		else
+			len += ft_putchar(*container);
+		container++;
+	}
+	return (len);
+}
+
+int	main(void)
+{
+	size_t len_mine = ft_printf("%s: %d\nThis is a null line: %s\n", "print my nummber", -42, NULL);
+	size_t len_orig = printf("%s: %d\nThis is a null line: %s\n", "print my nummber", -42, NULL);
+
+	printf("len mine: %zu\nlen orig: %zu\n", len_mine, len_orig);
+	return (0);
+}
